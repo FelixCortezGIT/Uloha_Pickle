c25d75f6538cc6ffae0682bab23bfd0c50cbacb9
TomasMacula <tomasmacula@live.com>
2026-02-05 16:29:40 +0100

    zaciatok projektu v pycharme a gite

---
+++ main.py
@@ -0,0 +1,28 @@
+class Cities:
+    def __init__(self, country, city):
+        self.country = country
+        self.city = city
+
+    # def add(self):
+    #
+    # def remove(self):
+    #
+    # def find(self):
+    #
+    # def edit(self):
+    #
+    # def save(self):
+    #
+    # def load(self):
+    #
+    #
+
+countries = {
+    "Slovensko": "Bratislava",
+    "Cesko": "Praha",
+    "Spojene Kralovstvo": "London",
+    "Nemecko": "Berlin",
+    "Francuzsko": "Paris"
+}
+
+print(countries["Slovensko"])
