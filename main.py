00e400469482f524d1026770323edd7baf5ce5a0
TomasMacula <tomasmacula@live.com>
2026-02-05 17:29:45 +0100

    pridanie funkcii, prevedenie zznamu di __init__ v triede, testovacie menu

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-class Cities:
-    def __init__(self, country, city):
-        self.country = country
-        self.city = city
+class Countries:
+    def __init__(self):
+        self.cities = {
+            "Slovensko": "Bratislava",
+            "Cesko": "Praha",
+            "Spojene Kralovstvo": "London",
+            "Nemecko": "Berlin",
+            "Francuzsko": "Paris"
+        }
 
-    # def add(self):
-    #
-    # def remove(self):
-    #
-    # def find(self):
-    #
-    # def edit(self):
-    #
+    def add(self, country, city):
+        self.cities[country] = city
+    def remove(self, country):
+        removed = self.cities.pop(country, None)
+        if removed is None:
+            print("krajina sa nenasla")
+        else:
+            print(f"krajina {country} bola odstranena")
+    def find(self, country):
+        return self.cities.get(country, "krajina sa nenasla")
+    def show_all(self):
+        for country, city in self.cities.items():
+            print(country, "->", city)
+    def edit(self, country, new_country, new_city):
+        if country not in self.cities:
+            print("krajina sa nenasla")
+        self.cities[new_country] = new_city
+        del self.cities[country]
+        print("udaje boli zmenene")
+
+
     # def save(self):
     #
     # def load(self):
     #
-    #
 
-countries = {
-    "Slovensko": "Bratislava",
-    "Cesko": "Praha",
-    "Spojene Kralovstvo": "London",
-    "Nemecko": "Berlin",
-    "Francuzsko": "Paris"
-}
-
-print(countries["Slovensko"])
+world = Countries()
+print(world.find("Slovensko"))
+world.add("Hungary", "Pest")
+world.show_all()
+world.remove("Nemecko")
+world.show_all()
+world.edit("Hungary", "Madarsko", "Budapest")
+world.show_all()
