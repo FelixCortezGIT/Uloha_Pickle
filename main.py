04c8462da1c021a65f57474c4dabf128dec83ca1
TomasMacula <tomasmacula@live.com>
2026-02-07 13:31:36 +0100

    pridane interaktivne menu, funkcia find nevracia chybove hlasenie ak nic nenajde a nic nevypise ak najde, funkcia find_by_city nevrati nic ked najde

--- main.py
+++ main.py
@@ -41,18 +41,80 @@ class Countries:
             self.cities = pickle.load(file)
 
 
-world = Countries()
-print(world.find("Slovensko"))
-world.add("Hungary", "Pest")
-world.show_all()
-world.remove("Nemecko")
-world.show_all()
-world.edit("Hungary", "Madarsko", "Budapest")
-world.show_all()
-print(world.find_by_city("Bratislava"))
-world.save()
-world.add("Canada", "Ottawa")
-world2 = Countries()
-world2.load()
-print(world2.cities)
-world.show_all()
+def wait_enter():
+    input("\npokracuj stlacenim enter...")
+
+def menu():
+    world = Countries()
+    while True:
+        print("1) pridaj")
+        print("2) odober")
+        print("3) zmen udaje")
+        print("4) najdi podla krajiny")
+        print("5) najdi podla mesta")
+        print("6) ukaz cely zoznam")
+        print("7) uloz do suboru capitals.pkl")
+        print("8) nacitaj zo suboru capitals.pkl")
+        print("0) koniec")
+        choice = input("tvoja volba: ")
+        if choice == "1":
+            new_country = input("zadaj nazov krajiny: ").strip()
+            new_city = input("zadaj nazov mesta: ").strip()
+            world.add(new_country, new_city)
+            print(f"krajina {new_country} s hlavnym mestom {new_city} boli pridane do zoznamu")
+            wait_enter()
+        elif choice == "2":
+            country = input("odstran krajinu: ")
+            world.remove(country)
+            wait_enter()
+        elif choice == "3":
+            country = input("krajinu ktoru chces prepisat: ")
+            new_country = input("nazov novej krajiny: ")
+            new_city = input("nazov noveho mesta: ")
+            world.edit(country, new_country, new_city)
+            wait_enter()
+        elif choice == "4":
+            country = input("ktoru krajinu hladas: ")
+            world.find(country)
+            wait_enter()
+        elif choice == "5":
+            city = input("hladaj mesto: ")
+            world.find_by_city(city)
+            wait_enter()
+        elif choice == "6":
+            world.show_all()
+            wait_enter()
+        elif choice == "7":
+            world.save()
+            print("zoznam bol ulozeny")
+            wait_enter()
+        elif choice == "8":
+            world.load()
+            print("zoznam bol nacitany")
+            wait_enter()
+        elif choice == "0":
+            print("koniec")
+            break
+        else:
+            print("neplatna volba")
+            wait_enter()
+
+menu()
+
+
+#
+# world = Countries()
+# print(world.find("Slovensko"))
+# world.add("Hungary", "Pest")
+# world.show_all()
+# world.remove("Nemecko")
+# world.show_all()
+# world.edit("Hungary", "Madarsko", "Budapest")
+# world.show_all()
+# print(world.find_by_city("Bratislava"))
+# world.save()
+# world.add("Canada", "Ottawa")
+# world2 = Countries()
+# world2.load()
+# print(world2.cities)
+# world.show_all()
